3712eb8a29bcbc3a59967efa6562a57d453d0fdd
Naveen Attri <nvn.atri@gmail.com>
2020-12-22 09:40:30 +0530

    Adding selection sort

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import LinkedListInit from "./linkedList";
 import StackInit from "./Stack";
 import BinaryTreeInit from "./BinaryTree";
 import BubbleSort from "./sorting/bubbleSort";
+import SelectionSort from "./sorting/selectionSort";
 
 // Uncomment to check logs for specific implementation
 // LinkedListInit();
 // StackInit();
 // BinaryTreeInit();
 // console.log(BubbleSort([1, 12, 7, 9, 10, 13, 14]));
+// console.log(SelectionSort([1, 12, 4, 9, 10, 13, 14]));
---
+++ src/sorting/selectionSort.js
@@ -0,0 +1,20 @@
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      swap(arr, min, i);
+    }
+  }
+  return arr;
+}
+
+export default selectionSort;
